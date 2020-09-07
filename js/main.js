61e608c18e954ce5dd59a7efb2cde1244d35a03f
Heber Gustavo <heber.gbarbosa@hotmail.com>
2020-09-07 16:50:39 -0300

    Finalização da versão web

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
-﻿//Efeito do backgroud ao usar o scroll
+﻿$(document).ready(function () {
+    AOS.init();
+});
+
+//Efeito do backgroud ao usar o scroll
 $(window).scroll(function () {
     var $height = $(window).scrollTop();
 
     if ($height > 50) {
         $('header nav').addClass('active-nav');
+        $('header nav a').addClass('active-cores');
         $('header nav .logo img').addClass('active-logo');
+
+        $('#logo-branca').addClass('logo-branca');
+        $('#logo-preta').addClass('logo-preta');
 
     } else {
         $('header nav').removeClass('active-nav');
+        $('header nav a').removeClass('active-cores');
         $('header nav .logo img').removeClass('active-logo');
         $('header nav .logo img').addClass('desative-logo');
+
+        $('#logo-branca').removeClass('logo-branca');
+        $('#logo-preta').removeClass('logo-preta');
     }
 });
 
@@ -20,7 +32,7 @@ $(document).on("click", 'a[href*="#"]', function (e) {
     var scrollToPosition = $(target).offset().top - 90;
 
     $('html,body').animate({ 'scrollTop': scrollToPosition }, 1000, function () {
-        
+
     });
     window.location.hash = target;
 
